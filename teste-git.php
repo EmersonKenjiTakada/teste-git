11b944a0e049536bab639c4c1a966a82be846cd8
EmersonKenjiTakada <84946887+EmersonKenjiTakada@users.noreply.github.com>
2024-01-19 21:02:01 -0300

    Inicializamos o projeto

---
+++ teste-git.php
@@ -0,0 +1,21 @@
+<?php 
+
+/*
+---------------------------------------------------------
+Plugin Name: Teste de GitHub
+Plugin URI: https://lojanegociosdigital.com.br/
+Author: Emerson Takada
+Author URI: https://lojanegociosdigital.com.br/
+Description: 
+Version: 1.0.0
+Requires at least: 3.5.0
+Tested up to: 6.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html 
+------------------------------------------------------------
+*/
+
+if (!defined('ABSPATH'))
+{
+    exit();
+}
